8edc5fa9e91826e49c204f4e90cdd5c7e81b2c4b
kostenk0 <tnellsu@gmail.com>
2020-06-22 15:55:24 +0300

    Updates

---
+++ precache-manifest.ba09350986918421ef09920ab647617b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ff8bec9d04ab3a9b18e6e44377b0d6",
+    "url": "/VakSight-web/index.html"
+  },
+  {
+    "revision": "25f2b04659eea42a4eef",
+    "url": "/VakSight-web/static/css/main.207df65c.chunk.css"
+  },
+  {
+    "revision": "263115890e57f92b9045",
+    "url": "/VakSight-web/static/js/2.1f8dec1d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/VakSight-web/static/js/2.1f8dec1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f2b04659eea42a4eef",
+    "url": "/VakSight-web/static/js/main.e5aaca2a.chunk.js"
+  },
+  {
+    "revision": "1db51afa9a297b7a1af4",
+    "url": "/VakSight-web/static/js/runtime-main.7d6b65cf.js"
+  }
+]);
